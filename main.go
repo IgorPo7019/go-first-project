36b24f478c9dc371a04be781890274cad2881afd
Igor P <slavstreet3070@rambler.ru>
2026-01-19 09:58:12 +0300

    Добавил fmt.Println в ветку feature-hello

--- main.go
+++ main.go
@@ -7,5 +7,6 @@ fmt.Println("Hello Git")
 fmt.Println("Привет Git!")
 fmt.Println("Изучаю Git и GitHub - commit 3")
 fmt.Println("Будет делать практический кейс по коммиту 4")
+fmt.Println("Привет из новой ветки feature-hello делаем коммит 5")
 
 }
